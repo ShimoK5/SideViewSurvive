20f15d46fca9f4ff37bc43b0515d2910758ea025
kunnnku <99231736+kunnnku@users.noreply.github.com>
2023-10-18 04:22:19 +0900

    <変更>StrageIconスクリプトの短縮に成功したため、変更しました。

--- Assets/Scenes/Sugawara/Script/StrageIcon.cs
+++ Assets/Scenes/Sugawara/Script/StrageIcon.cs
@@ -6,47 +6,27 @@ using UnityEngine.UI;
 public class StrageIcon : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]int HierarchyNumber = 3;                //どこのヒエラルキー階層に入れるか
     [SerializeField] GameObject PrefavIcon;                 //プレハブ化したオブジェクトを入れるよう
     [SerializeField] Sprite[] IconSprite = default;         //アイコン画像保管用
-    Vector3 PrefavePosition = Vector3.zero;                 //各プレハブごとの座標
     GameObject CloneIconBox = null;                         //生成したオブジェクトを入れる用
 
     void Start()
     {
-        PrefavePosition = this.transform.localPosition;         
-        int size = (IconSprite.Length/2);       
-        RectTransform IconBoxTransform = this.GetComponent<RectTransform>();
-        float IconPosition = (IconBoxTransform.sizeDelta.x /  2.0f )/ (1.0f+(float)size);
-        float MovePosition = IconPosition;
+        Vector3 PrefavePosition = this.transform.localPosition;     //各プレハブごとの座標
+        float IconPosition = (this.GetComponent<RectTransform>().sizeDelta.x - (PrefavIcon.GetComponent<RectTransform>().sizeDelta.x)) / (IconSprite.Length - 1);
+        float MovePosition = IconPosition;     
 
-        if((IconSprite.Length %2) == 1)
+        for (int i = 0; i < IconSprite.Length; i++)
         {
 
-            for (int i = 0; i<size;i++)
-            { 
-                if(i == 0)
-                {
-                    CloneIconBox = Instantiate(PrefavIcon, PrefavePosition, Quaternion.identity);
-                    CloneIconBox.GetComponent<Image>().sprite = IconSprite[size];
-                    CloneIconBox.transform.SetParent(GameObject.Find("SetRhythmUI").transform, false);                  
-                    CloneIconBox.transform.SetSiblingIndex(5);
-                }
-                PrefavePosition.x = IconPosition;
-                CloneIconBox = Instantiate(PrefavIcon, PrefavePosition, Quaternion.identity);
-                CloneIconBox.GetComponent<Image>().sprite = IconSprite[i];
-                CloneIconBox.transform.SetParent(GameObject.Find("SetRhythmUI").transform, false);               
-                CloneIconBox.transform.SetSiblingIndex(5);
+            PrefavePosition.x = ((-this.GetComponent<RectTransform>().sizeDelta.x + (PrefavIcon.GetComponent<RectTransform>().sizeDelta.x)) / 2)
+                + IconPosition * i;
+            CloneIconBox = Instantiate(PrefavIcon, PrefavePosition, Quaternion.identity);
+            CloneIconBox.GetComponent<Image>().sprite = IconSprite[i];
+            CloneIconBox.transform.SetParent(GameObject.Find("SetRhythmUI").transform, false);
+            CloneIconBox.transform.SetSiblingIndex(HierarchyNumber);
 
-                PrefavePosition.x = -IconPosition;
-                CloneIconBox = Instantiate(PrefavIcon, PrefavePosition, Quaternion.identity);
-                CloneIconBox.GetComponent<Image>().sprite = IconSprite[size + i + 1];
-                CloneIconBox.transform.SetParent(GameObject.Find("SetRhythmUI").transform, false);               
-                CloneIconBox.transform.SetSiblingIndex(5);
-
-                IconPosition += MovePosition;
-               
-
-            }
         }
     }
 
