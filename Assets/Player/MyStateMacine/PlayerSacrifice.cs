0d7f6d4309fbe53ab35659aa4612a1b5bfcdbe5a
MSI\clclm <hal88k5@gmail.com>
2023-10-17 12:00:51 +0900

    補完前

--- Assets/Player/MyStateMacine/PlayerSacrifice.cs
+++ Assets/Player/MyStateMacine/PlayerSacrifice.cs
@@ -13,7 +13,7 @@ public class PlayerSacrifice : PlayerIF
         //プレハブ生成
         GameObject Sacrifice = (GameObject)Resources.Load("Sacrifice");
         Sacrifice = Instantiate(Sacrifice, tf.transform.position,Quaternion.Euler(tf.transform.localEulerAngles));
-        Sacrifice.GetComponent<Sacrifice>().SetVel(new Vector2(tf.forward.x * 0.2f, 0.2f));
+        Sacrifice.GetComponent<Sacrifice>().SetVel(new Vector2(tf.forward.x * 0.07f, 0.4f));
     }
 
     public override void CustumUpdate()
