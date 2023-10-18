0d5a1214edeb8f630c92ab10c8510f8ca07e4507
kunnnku <99231736+kunnnku@users.noreply.github.com>
2023-10-18 09:07:59 +0900

    <変更>MoveHandスクリプトを一部変更しました。

--- Assets/Scenes/Sugawara/Script/CreateNoteBox.cs
+++ Assets/Scenes/Sugawara/Script/CreateNoteBox.cs
@@ -7,9 +7,7 @@ public class CreateNoteBox : MonoBehaviour
 {
     [SerializeField] GameObject PrefabNote = null;
     [SerializeField] GameObject[] SelectNote = new GameObject[8];
-    [SerializeField] int HierarchyNumber = 3;
-
-   
+    [SerializeField] int HierarchyNumber = 3;   
 
     void Start()
     {
@@ -21,7 +19,7 @@ public class CreateNoteBox : MonoBehaviour
             PrefabPosition.x = (-(this.GetComponent<RectTransform>().sizeDelta.x) + PrefabNote.GetComponent<RectTransform>().sizeDelta.x * 2 + 20.0f) / 2
                 + NotePosition * NoteNum;
             CloneNote = Instantiate(PrefabNote, PrefabPosition, Quaternion.identity);
-            CloneNote.name = "Note" + (NoteNum + 1);
+            //CloneNote.name = "Note" + (NoteNum + 1);
             CloneNote.transform.SetParent(GameObject.Find("SetRhythmUI").transform, false);
             CloneNote.transform.SetSiblingIndex(HierarchyNumber);
             MoveHand.instance.NoteBox[NoteNum] = CloneNote;
--- Assets/Scenes/Sugawara/Script/MoveHand.cs
+++ Assets/Scenes/Sugawara/Script/MoveHand.cs
@@ -9,7 +9,8 @@ public class MoveHand : MonoBehaviour
     [SerializeField] private Vector3 HandPosition = new Vector3(50.0f, -82.0f, 0.0f);       //ハンドの初期位置
     [SerializeField] private float MoveSpeed = 0.1f;                                        //移動スピード
     [SerializeField] private RectTransform Icon_Transform = null;                           //アイコンの位置、話したときに戻す用
-    [SerializeField] private bool Catch_Icon_Judge = false;                                 //アイコンを掴めるかどうかの確認
+    [SerializeField] private bool DragJudge = false;
+    [SerializeField] private bool DragAndDrop = false;                                 //アイコンを掴めるかどうかの確認
     [SerializeField] private GameObject Catch_Icon = null;                                  //アイコン保管用（多分消せる）
     [SerializeField] private Vector3 Catch_IconPosition = Vector3.zero;                     //移動する際に使用する位置座標（後で変数にして削除予定）
     public static MoveHand instance;
@@ -26,36 +27,32 @@ public class MoveHand : MonoBehaviour
         HandPosition = new Vector3(50.0f, -82.0f, 0.0f);
         Icon_Transform = this.GetComponent<RectTransform>();
         Icon_Transform.TransformPoint(HandPosition);
-        Catch_Icon_Judge = false;
+        DragAndDrop = false;
         
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("OnCollisionEnter2D: " + collision.gameObject.name);      //確認用　あとで削除予定
+        if (DragAndDrop == false && collision.gameObject.name == "Icon(Clone)")
+        {
+            DragJudge = true;
+            Catch_Icon = collision.gameObject;
+            Catch_IconPosition = Catch_Icon.GetComponent<RectTransform>().localPosition;
+        }
+        else if (DragAndDrop == false && collision.gameObject.name == "NextStage")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            DragJudge = false;
+        }
     }
 
     void OnTriggerStay2D(Collider2D collision)
     {
-        if (Input.GetKeyDown("joystick button 1"))
-        {
-            Debug.Log("OnCollisionEnter2D: " + collision.gameObject.name);
-            if (Catch_Icon_Judge == false && collision.gameObject.name == "Icon (4)(Clone)")
-            {
-                Catch_Icon_Judge = true;
-                Catch_Icon = collision.gameObject;
-                Catch_IconPosition = Catch_Icon.GetComponent<RectTransform>().localPosition;
-            }
-            else if (Catch_Icon_Judge == false && collision.gameObject.name == "NextStage")
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
-            else
-            {
-                Catch_Icon_Judge = false;
-            }
-
-        }
+        
     }
 
     void OnTriggerExit2D(Collider2D collision)
@@ -72,53 +69,75 @@ public class MoveHand : MonoBehaviour
     {
         if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)  //  テンキーや3Dスティックの入力（GetAxis）がゼロの時の動作
         {
-            //  なにもしない
+            if (DragJudge == true)
+            {
+                if (Input.GetKeyDown("joystick button 1"))
+                {
+                    DragAndDrop = true;
+                }
+            }
         }
         else //  テンキーや3Dスティックの入力（GetAxis）がゼロではない時の動作
         {
-            if (Input.GetAxis("Horizontal") > 0)
+            if (DragJudge == true)
             {
-                HandPosition.x = HandPosition.x + MoveSpeed;
-                if(Catch_Icon_Judge == true)
+                if (Input.GetKeyDown("joystick button 1"))
                 {
-                    Catch_IconPosition.x += MoveSpeed;
-                }
-                
-            }
-            else if (Input.GetAxis("Horizontal") < 0)
-            {
-                HandPosition.x = HandPosition.x + MoveSpeed * -1.0f;
-                if (Catch_Icon_Judge == true)
-                {
-                    Catch_IconPosition.x += MoveSpeed * -1.0f;
-                }
-            }
+                    DragAndDrop = true;
 
-            if (Input.GetAxis("Vertical") > 0)
-            {
-                HandPosition.y = HandPosition.y + MoveSpeed;
-                if (Catch_Icon_Judge == true)
-                {
-                    Catch_IconPosition.y += MoveSpeed;
+
                 }
             }
-            else if (Input.GetAxis("Vertical") < 0)
-            {
-                HandPosition.y = HandPosition.y + MoveSpeed * -1.0f;
-                if (Catch_Icon_Judge == true)
-                {
-                    Catch_IconPosition.y += MoveSpeed * -1.0f;
-                }
-            }
+            MoveChange();
             //HandPosition = new Vector3(HandPosition.x + MoveSpeed * Input.GetAxis("Vertical"), HandPosition.y + MoveSpeed * Input.GetAxis("Horizontal"), 0.0f);
             Icon_Transform.localPosition = HandPosition;
-            if (Catch_Icon_Judge == true)
+            if (DragAndDrop == true)
             {
                 Catch_Icon.GetComponent<RectTransform>().localPosition = Catch_IconPosition;
+            }           
+        }        
+    }
+
+    void MoveChange()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            HandPosition.x = HandPosition.x + MoveSpeed;
+            if (DragAndDrop == true)
+            {
+                Catch_IconPosition.x += MoveSpeed;
             }
-           
+
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            HandPosition.x = HandPosition.x + MoveSpeed * -1.0f;
+            if (DragAndDrop == true)
+            {
+                Catch_IconPosition.x += MoveSpeed * -1.0f;
+            }
         }
 
-        
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            HandPosition.y = HandPosition.y + MoveSpeed;
+            if (DragAndDrop == true)
+            {
+                Catch_IconPosition.y += MoveSpeed;
+            }
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            HandPosition.y = HandPosition.y + MoveSpeed * -1.0f;
+            if (DragAndDrop == true)
+            {
+                Catch_IconPosition.y += MoveSpeed * -1.0f;
+            }
+        }
+    }
+
+    void DoCarryIcon()
+    {
+
     }
 }
