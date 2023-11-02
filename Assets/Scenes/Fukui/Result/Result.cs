17662eb76c6f56f7946d62f11e265b7be1167868
Tfukui <t.f.shioeazd0129@gmail.com>
2023-11-02 13:14:02 +0900

    セットシーンに遷移

--- Assets/Scenes/Fukui/Result/Result.cs
+++ Assets/Scenes/Fukui/Result/Result.cs
@@ -16,7 +16,7 @@ public class Result : MonoBehaviour
     {
         if (Input.GetKeyDown("space") /*|| Input.GetButtonDown("Action1")*/) //スペースキー、Aボタン
         {
-            SceneManager.LoadScene("NewRecord");//some_senseiシーンをロードする
+            SceneManager.LoadScene("SetScene");
         }
     }
 }
