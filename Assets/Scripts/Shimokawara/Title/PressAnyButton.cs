0f552834f61473a8a9a45058ee98d74428766787
MSI\clclm <hal88k5@gmail.com>
2023-11-02 22:47:00 +0900

    タイトル画面制作

---
+++ Assets/Scripts/Shimokawara/Title/PressAnyButton.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PressAnyButton : MonoBehaviour
+{
+    RectTransform rtf;      //レクトトランスフォーム
+    Image img;              //イメージ
+
+
+    int NormalCnt = 0;
+    int WaitCnt = 0;
+
+    Vector3 DefaultPos;
+    Vector3 DefaultScale;
+    Color   DefaultColor;
+
+    Vector3 AnotherPos;
+    Vector3 AnotherScale;
+    Color   AnotherColor;
+
+    float WaitAlpha = 1.0f;
+    static float AlphaMoveRatio = 0.33f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NormalCnt = 0;
+        WaitCnt = 0;
+
+        rtf = GetComponent<RectTransform>();
+        img = GetComponent<Image>();
+
+        DefaultPos = rtf.position;
+        DefaultScale = rtf.localScale;
+        DefaultColor = img.color;
+        AnotherPos = DefaultPos + new Vector3(0, -20, 0);
+        AnotherScale = DefaultScale * 0.9f;
+        AnotherColor.r = DefaultColor.r * 0.6f;        AnotherColor.g = DefaultColor.g * 0.6f;        AnotherColor.b = DefaultColor.b * 0.6f;
+        AnotherColor.a = 1.0f;
+
+        WaitAlpha = 1.0f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (TitleStateManager.instans.state)
+        {
+            case TitleStateManager.State.Normal:
+                NormalFixed();
+                break;
+            case TitleStateManager.State.After:
+                AfterFixed();
+                break;
+
+        }
+
+    }
+
+    void NormalFixed()
+    {
+        NormalCnt++;
+
+        if((NormalCnt / 30) % 2 == 0)
+        {
+            rtf.position = DefaultPos;
+            rtf.localScale = DefaultScale;
+            img.color = DefaultColor;
+        }
+        else
+        {
+            rtf.position = AnotherPos;
+            rtf.localScale = AnotherScale;
+            img.color = AnotherColor;
+        }
+    }
+
+    void AfterFixed()
+    {
+        WaitCnt++;
+
+        rtf.position = DefaultPos;
+        rtf.localScale = DefaultScale;
+
+        if (WaitCnt >= TitleStateManager.instans.WAIT_MAX_CNT - 30)
+        {
+            WaitAlpha = Mathf.Min(WaitAlpha + AlphaMoveRatio, 1.0f);
+
+            
+            img.color = new Color(DefaultColor.r, DefaultColor.g, DefaultColor.b , WaitAlpha);
+        }
+
+        else
+        {
+            if ((WaitCnt / 5) % 2 == 0)
+            {
+                WaitAlpha = Mathf.Max(WaitAlpha - AlphaMoveRatio, 0.0f);
+                img.color = new Color(DefaultColor.r, DefaultColor.g, DefaultColor.b, WaitAlpha);
+            }
+            else
+            {
+                WaitAlpha = Mathf.Min(WaitAlpha + AlphaMoveRatio, 1.0f);
+                img.color = new Color(DefaultColor.r, DefaultColor.g, DefaultColor.b, WaitAlpha);
+            }
+
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Shimokawara/Title/TitleStateManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TitleStateManager : MonoBehaviour
+{
+    public enum State {
+    Normal,
+    After
+    }
+
+    public static TitleStateManager instans;
+
+    public int WAIT_MAX_CNT = 80;   //入力後の待ち時間
+    public State state;
+
+    int WaitCnt = 0;
+
+    void Awake()
+    {
+        instans = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WAIT_MAX_CNT = 80;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (state)
+        { 
+            case State.Normal:
+                if (Input.anyKeyDown)
+                {
+                    state = State.After;
+                }
+                break;
+            case State.After:
+                
+                break;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        switch (state)
+        {
+            case State.Normal:
+                
+                break;
+            case State.After:
+                WaitCnt++;
+                if (WaitCnt == WAIT_MAX_CNT)
+                {
+                    SceneManager.LoadScene("SetScene");
+                }
+                break;
+        }
+    }
+}
