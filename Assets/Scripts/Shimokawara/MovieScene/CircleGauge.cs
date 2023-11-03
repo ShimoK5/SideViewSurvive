757313c62cadbb8c759e1b1a5d98ec5753e592ad
MSI\clclm <hal88k5@gmail.com>
2023-11-03 20:53:31 +0900

    ムービーシーンに長押しスキップを追加

---
+++ Assets/Scripts/Shimokawara/MovieScene/CircleGauge.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleGauge : MonoBehaviour
+{
+    private Image image;
+
+    private float param = 0;
+
+    [SerializeField]
+    private float speed = 0.05f;
+
+    [SerializeField]
+    private string nextSceneName;
+
+    void Start()
+    {
+        image = gameObject.GetComponent<Image>();
+        image.fillAmount = param;
+    }
+
+    void FixedUpdate()
+    {
+        if (InputManager_FU.instanse.GetKey(Key.B))
+        {
+            if ((param += speed) >= 1)
+            {
+                SceneChangeManager.instance.SceneTransition(nextSceneName);
+                this.enabled = false;
+                //param = 0;
+            }
+        }
+        else
+        {
+            if (param > 0)
+            {
+                param = 0;
+            }
+        }
+        image.fillAmount = param;
+    }
+}
