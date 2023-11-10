dfc102b562afe9a6ae4d6341eab0a6fa67a3f885
MSI\clclm <hal88k5@gmail.com>
2023-11-10 15:11:08 +0900

    キャンバスモードをカメラに変更

---
+++ Assets/Scripts/Shimokawara/FindUICamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindUICamera : MonoBehaviour
+{
+    void Awake()
+    {
+        GetComponent<Canvas>().worldCamera = GameObject.FindWithTag("UICamera").GetComponent<Camera>();
+        GetComponent<Canvas>().planeDistance = 5;
+    }
+}
