545dece07b831ff8099fe57d14372f93db346656
MSI\clclm <hal88k5@gmail.com>
2023-11-21 16:35:10 +0900

    セットシーン時の不当エフェクト削除

--- Assets/Scripts/Shimokawara/SetScene/PlayerPosReseter.cs
+++ Assets/Scripts/Shimokawara/SetScene/PlayerPosReseter.cs
@@ -9,12 +9,15 @@ public class PlayerPosReseter : MonoBehaviour
 
     bool OverOnce = false;
 
+    ParticleSystem ps;
+
     Vector3 DefaultPos = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
         DefaultPos = Player.instance.transform.position;
+        ps = GameObject.Find("vfx_PlayerWalking").GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -28,7 +31,9 @@ public class PlayerPosReseter : MonoBehaviour
             OverOnce = true;
             if(Player.instance.transform.position.x > 100f)
             {
+                ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
                 Player.instance.transform.position = DefaultPos;
+                ps.Play(true);
             }            
         }
 
