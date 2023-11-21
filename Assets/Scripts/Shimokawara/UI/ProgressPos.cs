785306cb644798b22818e90ed8b665d6baac677b
MSI\clclm <hal88k5@gmail.com>
2023-11-21 20:51:45 +0900

    トゥーンシェ－ダーと進捗バー完成

---
+++ Assets/Scripts/Shimokawara/UI/ProgressPos.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressPos : MonoBehaviour
+{
+    float LocalMinX;
+    float LocalMaxX;
+
+    float WorldMinX;
+    float WorldMaxX;
+
+    Transform PlayerTF;
+
+    RectTransform ThisRTH;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LocalMinX = GameObject.Find("BarStart").GetComponent<RectTransform>().localPosition.x;
+        LocalMaxX = GameObject.Find("BarEnd").GetComponent<RectTransform>().localPosition.x;
+
+        WorldMinX = GameObject.Find("StartObj").transform.position.x;
+        WorldMaxX = GameObject.Find("GoalObj").transform.position.x;
+
+        PlayerTF = Player.instance.transform;
+        ThisRTH = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float PlayerProgressWariai
+            = (PlayerTF.position.x - WorldMinX) / (WorldMaxX - WorldMinX);
+
+        float RectLocalX = Mathf.Lerp(LocalMinX, LocalMaxX, PlayerProgressWariai);
+
+        ThisRTH.localPosition = new Vector3(RectLocalX, ThisRTH.localPosition.y, ThisRTH.localPosition.z);
+    }
+}
