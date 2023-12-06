3b99784ce27eef6551460e03e71b9b555047d9c0
MSI\clclm <hal88k5@gmail.com>
2023-12-06 10:31:46 +0900

    ランドセル修正

--- Assets/Editor/WorldPosCS.cs
+++ Assets/Editor/WorldPosCS.cs
@@ -1,21 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEditor;
-
-[CustomEditor(typeof(Transform))]       // こうすることによりTransformコンポーネントのインスペクタに、カスタム項目が追加できる
+[CustomEditor(typeof(Transform))]
+[CanEditMultipleObjects]
 public class WorldPosCS : Editor
 {
-    Transform _t = null;
+    private Type _transformRotationGUIType;
+    private System.Object _transformRotationGUIObject;
+    private MethodInfo _rotationFieldMethod;
+    private SerializedProperty _positionProperty;
+    private SerializedProperty _scaleProperty;
+    private Transform _transform;
 
     private void OnEnable()
     {
-        _t = target as Transform;
-    }
+        _transform = target as Transform;
+        _positionProperty = serializedObject.FindProperty("m_LocalPosition");
+        _scaleProperty = serializedObject.FindProperty("m_LocalScale");
 
+        _transformRotationGUIType = typeof(EditorApplication).Assembly.GetType("UnityEditor.TransformRotationGUI");
+        if (_transformRotationGUIObject == null)
+            _transformRotationGUIObject = Activator.CreateInstance(_transformRotationGUIType);
+
+        var enableMethod = _transformRotationGUIType
+            .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic)
+            .First(m => m.Name == "OnEnable");
+
+        // Initialize TransformRotationGUI
+        enableMethod.Invoke(_transformRotationGUIObject, new object[]
+        {
+            serializedObject.FindProperty("m_LocalRotation"),
+            EditorGUIUtility.TrTextContent("Rotation")
+        });
+
+        _rotationFieldMethod = _transformRotationGUIType
+            .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic)
+            .Where(m => m.Name == "RotationField")
+            .FirstOrDefault(m => m.GetParameters().Length == 0);
+    }
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
-        EditorGUILayout.Vector3Field("World Position", _t.position);       // ワールド座標を表示する
+        EditorGUILayout.PropertyField(_positionProperty);
+
+        _rotationFieldMethod.Invoke(_transformRotationGUIObject, null);
+
+        EditorGUILayout.PropertyField(_scaleProperty);
+
+        using (new EditorGUI.DisabledScope(true))
+        {
+            EditorGUILayout.LabelField("World (Read Only)");
+            EditorGUILayout.Vector3Field("Position", _transform.position);
+            EditorGUILayout.Vector3Field("Rotation", _transform.rotation.eulerAngles);
+            EditorGUILayout.Vector3Field("Scale", _transform.lossyScale);
+        }
     }
 }
