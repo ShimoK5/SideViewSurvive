03b5e8a863c224616740da5c2348eb9c13d0120e
史嵩 小島 <menou2234@gmail.com>
2023-12-08 15:38:45 +0900

    プレス基本挙動完成

--- Assets/Enemy/EnemyFallLeft.cs
+++ Assets/Enemy/EnemyFallLeft.cs
@@ -9,16 +9,52 @@ public class EnemyFallLeft : EnemyIF
         //EnemyAnim.instans.Anim.SetTrigger("Run");
         CopyEnemy(oldEnemy);
     }
-
-    // Start is called before the first frame update
-    void Start()
+    public override void CustumFixed()
     {
-        
+        //過去情報保存
+        KeepOld();
+        //勢い減少 
+        SlowDown(GROUND_VEL_MULTI, GROUND_VEL_MULTI);
+        //自由落下
+        //Fall();
+
+        //
+
+
+        //ジャンプ処理
+        //Jump();
+        //状態遷移
+        ChangeNextState();
+        //速度反映
+        MoveEnemy();
+        //フラグリセット
+        //JumpKeyDown = false;
+        //共通更新
+        FixedCommon();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void HitUnder(Block block)
     {
-        
+        //Debug.Log("床");
+        isGround = true;
+        StandBlock = block;
+        float YPos = block.transform.position.y + (block.Size.y + Size.y) / 2;
+        tf.transform.position = new Vector3(tf.transform.position.x, YPos, tf.transform.position.z);
+        SelfVel.y = 0.0f;
+        OtherVel.y = 0.0f;
     }
+
+    //状態遷移
+    protected override void ChangeNextState()
+    {
+        //if (Mathf.Abs(SelfVel.x/* + OtherVel.x*/) < STAND_SPEED && NextEnemyState == ENEMY_STATE.RUN)
+        //{
+        //    NextEnemyState = ENEMY_STATE.STAND;
+        //}
+        //if (!isGround)
+        //{
+        //    NextEnemyState = ENEMY_STATE.AIR;
+        //}
+    }
+
 }
