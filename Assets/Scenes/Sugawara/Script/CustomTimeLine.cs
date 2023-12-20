38f3f3f8c18b1cd987f3b00fc088b9a6026fc0eb
kunnnku <99231736+kunnnku@users.noreply.github.com>
2023-12-20 17:11:44 +0900

    <変更>セットUI一部変更と、ビルドの際にエラーが出る一部の変更

--- Assets/Scenes/Sugawara/Script/CustomTimeLine.cs
+++ Assets/Scenes/Sugawara/Script/CustomTimeLine.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
-using static Data.Util.KeywordDependentCollection;
 
 public class CustomTimeLine : MonoBehaviour
 {
