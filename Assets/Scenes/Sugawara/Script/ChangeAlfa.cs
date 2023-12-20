f3263298c1fd6ade527f24437e0b71645fcbc14b
kunnnku <99231736+kunnnku@users.noreply.github.com>
2023-12-20 16:48:11 +0900

    <変更・追加>映像用の自分のゲームシーンの追加。プレビュー・ヘッダー・初心者・中級者用のテキストとイメージの追加

---
+++ Assets/Scenes/Sugawara/Script/ChangeAlfa.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeAlfa : MonoBehaviour
+{
+    Color TextColor = Color.black;
+    float ChangeSpeed = 0.04f;
+    bool Change = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextColor.a = 0.0f;
+        this.gameObject.GetComponent<Text>().color = TextColor;
+        Change = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Change == false)
+        {
+            TextColor.a += ChangeSpeed;
+            if (TextColor.a > 1.0f)
+            {
+                TextColor = Color.black;
+            }
+            this.GetComponent<Text>().color = TextColor;
+        }
+        else
+        {
+
+        }
+    }
+}
