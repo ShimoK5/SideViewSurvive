4c8ee0edf9b65c82cc61019c64c6fae2f3f21157
kunnnku <99231736+kunnnku@users.noreply.github.com>
2024-01-25 11:37:07 +0900

    <追加>サウンドマネージャーの変更・セット画面のBGM再生

---
+++ Assets/Scenes/Sugawara/Script/PlayBGM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBGM : MonoBehaviour
+{
+    bool PlaySound = false;
+    void Awake()
+    {
+        PlaySound = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (PlaySound == false)
+        {
+            NewSoundManager.instance.PlayBGM("セットBGM");
+            PlaySound = true;
+        }
+    }
+}
