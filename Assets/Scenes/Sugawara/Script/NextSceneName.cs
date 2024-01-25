d99e6f3749d7d552b2832365503df711a68d638f
kunnnku <99231736+kunnnku@users.noreply.github.com>
2024-01-25 09:48:28 +0900

    <変更>ビルドの際にエラー部分の修正

--- Assets/Scenes/Sugawara/Script/NextSceneName.cs
+++ Assets/Scenes/Sugawara/Script/NextSceneName.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static Data.Util.KeywordDependentCollection;
 
 public class NextSceneName : MonoBehaviour
 {
